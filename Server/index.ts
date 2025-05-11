97f424b007aa72f6635910618c66587f2006914e
Hai-Ho-1990 <juno.hu2090@gmail.com>
2025-05-11 18:40:04 +0200

    test deploy

--- Server/index.ts
+++ Server/index.ts
@@ -1,14 +1,11 @@
-import express from 'express'; // Importerar express
-import path from 'path'; // Importerar path-modulen
+import express, { Request, Response } from 'express';
 import cors from 'cors';
-import pool from './db.js'; // Om din db är en JS-fil efter kompilering
+import pool from './db.js';
+
 import authRoutes from './routes/authRoutes.js';
 
 const app = express();
 const port = process.env.PORT || 5000;
-
-// Få den aktuella katalogen via import.meta.url
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
 app.use(cors());
 app.use(express.json());
@@ -18,11 +15,8 @@ app.use('/api', authRoutes);
 
 app.use(express.urlencoded({ extended: true }));
 
-// Servera statiska filer från dist
-app.use(express.static(path.join(__dirname, 'dist')));
-
 // Standard endpoint för att testa servern
-app.get('/api', async (req, res) => {
+app.get('/api', async (req: Request, res: Response) => {
     const result = await pool.query('SELECT NOW()');
     res.json(result.rows[0]);
 });
